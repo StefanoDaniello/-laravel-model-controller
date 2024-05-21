66054233dbfd46ad030982cb97d9a3c3a605ff1e
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-05-21 17:25:14 +0200

    frooter

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,13 @@
-<footer>
-    <h1>footer</h1>
+<footer class="bg-dark text-white">
+    <p class="mt-3 mb-0">&copy; {{ date('Y') }}. All Rights Reserved.</p>
 </footer>
+
+
+<style lang="scss" scoped>
+   footer{
+       height: 100px;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+   }
+</style>
